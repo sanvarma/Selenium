6cd253cb4bc604a8b4fadfa4e78ac63811b841fc
sandeep <sandeep@sandeeps-MacBook-Air.local>
2018-09-17 20:55:27 +0530

    inital

---
+++ google.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+# cap = DesiredCapabilities.FIREFOX.update({["platform"] :"LINUX"}    )
+binary = FirefoxBinary("/Users/sandeep/Downloads/geckodriver")
+#driver = webdriver.Chrome(executable_path='/Users/sandeep/Downloads/chromedriver')
+driver = webdriver.Remote(command_executor='127.0.0.1:4446/wd/hub',desired_capabilities=DesiredCapabilities.CHROME)
+
+driver.get("https://www.google.com")
+time.sleep(5)
+ele = driver.find_element(By.XPATH, '//*[@href="https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/"]')
+f = open("/Users/sandeep/Downloads/Testing.txt", "w+")
+if ele.is_displayed() == True :
+    f.write(" PASS ")
+else:
+    f.write(" FAIL ")
+
+f.close()
+
+driver.quit()
+
